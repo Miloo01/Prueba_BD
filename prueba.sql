9a98b86df5081d70904c02f55f26beef32adb0e8
Camilo Monsalve <cmonsalsilva@gmail.com>
2020-09-04 13:36:26 -0400

    ajusterrores+datostabla

--- prueba.sql
+++ prueba.sql
@@ -6,7 +6,7 @@ CREATE DATABASE prueba;
 --\c prueba;
 
 --TABLA FACTURAS
-CREATE TABLE facturas(id SERIAL INT PRIMARY KEY, fecha_de_factura DATE, subtotal FLOAT NOT NULL, iva FLOAT NOT NULL, precio_total FLOAT NOT NULL);
+CREATE TABLE facturas(id SERIAL PRIMARY KEY, fecha_de_factura DATE, subtotal FLOAT NOT NULL, iva FLOAT NOT NULL, precio_total FLOAT NOT NULL);
 
 --TABLA CLIENTES
 CREATE TABLE clientes(id SERIAL PRIMARY KEY, nombre VARCHAR(100) UNIQUE,rut VARCHAR(15),direccion VARCHAR(150));
@@ -17,19 +17,38 @@ CREATE TABLE categorias(id SERIAL PRIMARY KEY, nombre VARCHAR(100) NOT NULL UNIQ
 -- TABLA PRODUCTOS
 CREATE TABLE productos(id SERIAL PRIMARY KEY, nombre VARCHAR(100),descripcion VARCHAR(150),valor_unitario FLOAT,categoria_id INT);
 
+-- TABLA FACPRO
+
+CREATE TABLE facpro(factura_id INT, producto_id INT, FOREIGN KEY(factura_id) REFERENCES facturas(id), FOREIGN KEY(producto_id) REFERENCES productos(id), cantidad INT);
 
 
+-- CLIENTES_FACTURA
+CREATE TABLE clientes_factura(cliente_id INT NOT NULL, factura_id INT NOT NULL, FOREIGN KEY(cliente_id) REFERENCES clientes(id), FOREIGN KEY(factura_id) REFERENCES facturas(id));
 
-
-
+-- POBLAR DATA INTO TABLES
 
 --5 clientes-
 
---8 productos--
+INSERT INTO clientes(nombre, rut, direccion) VALUES
+('Jaimito Ahumada',12122345-4, 'Nonato coo 7772,la florida'),
+('Viviana Castan', 14677543-9, 'Portugal 666, Santiago centro'),
+('Alex Silva', 17365321-1, 'Las tertutlias 345, depto 24, Las condes'),
+('Isa Humble', 22457123-8, 'Calle condamine 51, Balgowlah'),
+('Rose Doherty', 24776872-2, 'Avenida sunshine coast 21, Australia');
 
+--ver tabla SELECT * FROM clientes;
 
---3 categorias--
+--8 productos y 3 categorías(1=videojueos, 2=consolas, 3=peluches, 4=tecladosycomputacion)
 
+INSERT INTO productos(nombre, descripcion, valor_unitario,categoria_id) VALUES
+('Doom','juego de accion primera persona', 1000, 1),
+('Gran Turismo 5','simulacion de conduccion carreras', 2000, 1),
+('Play station 4','consola videojuegos sony', 10000, 2),
+(' Xbox X','consola videojuegos microsoft', 20000, 2),
+('Nintendo U','consola videojuegos nintendo', 5000, 2),
+('Capitan America','figura de accion', 500, 3),
+('Batman','figura de accion', 500, 3),
+('Teclado gamer','teclado ergonomico para videojuegos', 2000, 4);
 
 --10 facturas--
 
